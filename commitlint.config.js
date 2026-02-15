ae301d9fe1514f5eafe9613b8ece9722ab147f0b
Syed Sibteali Baqar <sibteali786@gmail.com>
2026-02-15 11:02:30 +0500

    feat: add husky with conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Code style changes (formatting, semicolons, etc)
+        "refactor", // Code refactoring
+        "perf", // Performance improvements
+        "test", // Adding or updating tests
+        "build", // Build system or dependencies
+        "ci", // CI/CD changes
+        "chore", // Other changes (maintenance, configs)
+        "revert", // Revert previous commit
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+  },
+};
